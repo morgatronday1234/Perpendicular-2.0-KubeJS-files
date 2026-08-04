1c8fd98d8e8343b89db35a19fb5db3edf62671d0
Morg <130381148+morgatronday1234@users.noreply.github.com>
2026-08-04 03:25:22 +0100

    I **created** the **create** file
    Please laugh I'm funny.
    
    (I'm running out of shit to say fast, This is going to turn into the HBM Repo with just confusing commit messages)

---
+++ kubejs/server_scripts/recipes/create.js
@@ -0,0 +1,34 @@
+// priority: 10
+
+// Reasons why something is commented out:
+// <M-R> == A mod was removed/not in pack, And this line is not-longer applicable.
+// <F-R> == Something that was from Forge, And is now something diffrent in Neoforge.
+
+ServerEvents.recipes(event => {
+
+    // -----------------
+    // Mixing
+    // -----------------
+    const MIXING_PREFIX = 'perpendicular:mixing/';
+
+    // Memory essence from items
+    event.recipes.create.mixing(Fluid.of('pneumaticcraft:memory_essence', 540), 'create:experience_block').heated().id(`${MIXING_PREFIX}memory_essence_from_experience_block`)
+    event.recipes.create.mixing(Fluid.of('pneumaticcraft:memory_essence', 60), 'ars_nouveau:experience_gem').heated().id(`${MIXING_PREFIX}memory_essence_from_experience_gem`)
+    event.recipes.create.mixing(Fluid.of('pneumaticcraft:memory_essence', 240), 'ars_nouveau:greater_experience_gem').heated().id(`${MIXING_PREFIX}memory_essence_from_greater_experience_gem`)
+
+    // Memory essence from other XP fluids
+    event.recipes.create.mixing(Fluid.of('pneumaticcraft:memory_essence', 1000), Fluid.of('enderio:xp_juice', 1000)).id(`${MIXING_PREFIX}memory_essence_from_xp_juice`)
+    event.recipes.create.mixing(Fluid.of('pneumaticcraft:memory_essence', 1000), Fluid.of('sophisticatedcore:xp_still', 1000)).id(`${MIXING_PREFIX}memory_essence_from_experience`)
+    //event.recipes.create.mixing(Fluid.of('pneumaticcraft:memory_essence', 1000), Fluid.of('cofh_core:experience', 1000)).id(`${MIXING_PREFIX}memory_essence_from_essence_of_knowledge`) //<M-R> 
+    //event.recipes.create.mixing(Fluid.of('pneumaticcraft:memory_essence', 1000), Fluid.of('industrialforegoing:essence', 1000)).id(`${MIXING_PREFIX}memory_essence_from_essence`) //<M-R> 
+
+    // Steel
+    //event.recipes.create.mixing('thermal:steel_ingot', ['minecraft:iron_ingot', '#crossroads:blast_furnace_carbon']).heated().id(`${MIXING_PREFIX}steel`) //(*Thermal stabs you in the back*)
+
+    // -----------------
+    // Milling
+    // -----------------
+    const MILLING_PREFIX = 'perpendicular:milling/';
+
+    //event.recipes.create.milling(['2x alexscaves:gingerbread_crumbs', Item.of('alexscaves:gingerbread_crumbs').withChance(0.5)], 'create_confectionery:gingerbread').id(`${MILLING_PREFIX}gingerbread_crumbs`); //<M-R> 
+})
