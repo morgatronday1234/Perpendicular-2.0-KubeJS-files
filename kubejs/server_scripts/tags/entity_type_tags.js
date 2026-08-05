30cad047db4c1f36e6d2fd16320a222ade8173b6
Morg <130381148+morgatronday1234@users.noreply.github.com>
2026-08-05 22:35:51 +0100

    I just removed everything...
    It all was removed mods... Im sorry ::/

---
+++ kubejs/server_scripts/tags/entity_type_tags.js
@@ -0,0 +1,92 @@
+// priority: 100
+
+ServerEvents.tags('entity_type', event => {
+  // Allow Alex's Caves mobs to survive in space
+  
+  /* Yet once more, Ad astra got removed. 
+  event.add('ad_astra:lives_without_oxygen', [
+    'minecraft:drowned',
+    'minecraft:bat',
+
+    // Primordial Caves (Mars)
+    'minecraft:frog',
+    'alexscaves:trilocaris',
+    'alexscaves:subterranodon',
+    'alexscaves:vallumraptor',
+    'alexscaves:grottoceratops',
+    'alexscaves:tremorsaurus',
+    'alexscaves:relicheirus',
+    'alexscaves:atlatitan',
+    'alexscaves:tremorzilla',
+    'alexscaves:luxtructosaurus',
+
+    // Magnetic Caves (Mercury)
+    'alexscaves:notor',
+    'alexscaves:magnetron',
+    'alexscaves:teletor',
+    'alexscaves:boundroid',
+    'alexscaves:boundroid_winch',
+    'alexscaves:ferrouslime',
+
+    // Forlorn Hollows (Moon)
+    'alexscaves:gloomoth',
+    'alexscaves:underzealot',
+    'alexscaves:corrodent',
+    'alexscaves:vesper',
+    'alexscaves:forsaken',
+    'alexscaves:watcher',
+
+    // Toxic Caves (Venus)
+    'alexscaves:gammaroach',
+    'alexscaves:raycat',
+    'alexscaves:brainiac',
+    'alexscaves:nucleeper',
+    'alexscaves:radgill'
+  ]);
+
+    event.add('ad_astra:can_survive_extreme_cold', [
+    'minecraft:drowned',
+    'minecraft:bat',
+
+    // Primordial Caves (Mars)
+    'minecraft:frog',
+    'alexscaves:trilocaris',
+    'alexscaves:subterranodon',
+    'alexscaves:vallumraptor',
+    'alexscaves:grottoceratops',
+    'alexscaves:tremorsaurus',
+    'alexscaves:relicheirus',
+    'alexscaves:atlatitan',
+    'alexscaves:tremorzilla',
+    'alexscaves:luxtructosaurus',
+
+    // Forlorn Hollows (Moon)
+    'alexscaves:gloomoth',
+    'alexscaves:underzealot',
+    'alexscaves:corrodent',
+    'alexscaves:vesper',
+    'alexscaves:forsaken',
+    'alexscaves:watcher'
+  ]);
+
+  event.add('ad_astra:can_survive_extreme_heat', [
+    'minecraft:drowned',
+    'minecraft:bat',
+
+    // Magnetic Caves (Mercury)
+    'alexscaves:notor',
+    'alexscaves:magnetron',
+    'alexscaves:teletor',
+    'alexscaves:boundroid',
+    'alexscaves:boundroid_winch',
+    'alexscaves:ferrouslime',
+
+    // Toxic Caves (Venus)
+    'alexscaves:gammaroach',
+    'alexscaves:raycat',
+    'alexscaves:brainiac',
+    'alexscaves:nucleeper',
+    'alexscaves:radgill'
+  ]);
+  */
+});
